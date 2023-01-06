4db16664c394840ff204f44c1f2f7f0b44a9ce57
aicaicaicai <1507007177@qq.com>
2023-01-06 14:18:35 +0800

    修改配置文件

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ HOSTNAME = '127.0.0.1'
 PORT = '3306'
 DATABASE = 'mqttc01'
 USERNAME = 'root'
-PASSWORD = 'Syj123456'
+PASSWORD = 'Public'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 
