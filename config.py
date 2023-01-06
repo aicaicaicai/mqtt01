13cb95378d224bc7c5c18bcff077fbfae1b1da72
aicaicaicai <1507007177@qq.com>
2023-01-06 14:21:05 +0800

    修改配置文件

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ SECRET_KEY = "mdofckc%emqxflask;ok"
 # 数据库配置信息
 HOSTNAME = '127.0.0.1'
 PORT = '3306'
-DATABASE = 'mqttc01'
+DATABASE = 'mqtt01'
 USERNAME = 'root'
 PASSWORD = 'Public'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
